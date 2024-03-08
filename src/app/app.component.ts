954c7a788167f8b367406b5fd9c0cfd0a5304b84
unknown <G00417466@atu.ie>
2024-03-08 13:29:53 +0000

    Lab 5 - API and Pulling Data Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StudentService } from './Services/student.service'; //Need to import our service to get the data it retrieves
+import { CommonModule } from '@angular/common';
+import { WeatherService } from './Services/weather.service'; //Need to import our weather service before we can use it
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'data-app';
+
+export class AppComponent implements OnInit {
+  students:any = [];
+  weatherInfo:any = [];
+  weatherDetails:any = ""; //Other two are arrays of details so they can be stored in arrays, since this isnt an array and is an object
+  //we need to store it in an empty string
+  constructor(private studentService:StudentService, private weatherService:WeatherService){} // <-- Exam Question
+
+  //Safer place to put our logic than in a constructor - must first include the OnInit in the Component Import and implement it in this clas
+  ngOnInit(): void {
+    this.studentService.GetStudentData().subscribe(
+      (data)=>{
+        //Retrieves each of the students objects and stores them in our any array
+        this.students = data.students;
+      }
+    );
+
+    //Now we get the info and store it in an array for the proper usage
+    this.weatherService.GetWeather().subscribe(
+      (data)=>{
+        this.weatherInfo = data.weather;
+        this.weatherDetails = data.main;
+      }
+    );
+  }
+  //Subscribe will allow us to run and get data asynchronously 
 }
